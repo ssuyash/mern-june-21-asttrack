9662c1f32fbf2b369af615ee45c5e746b4ccae7f
ssuyash <kumarsuyash94@gmail.com>
2021-06-22 10:07:13 +0530

    document

---
+++ dom/script.js
@@ -0,0 +1,39 @@
+//document => dom
+
+console.log(document.title)
+console.log(document.URL)//read only
+console.log(document.location)
+console.log(document.location.href)
+//document.location = "http://google.com"
+console.log(document.domain)
+console.log(document.lastModified)
+console.log(document.readyState)
+console.log(document.body) // returns body node 
+console.log(document.characterSet)
+console.log(document.links)
+console.log(document.cookie)
+console.log(document.referer)
+
+
+setTimeout(function(){
+    console.log(document.readyState)
+}, 1000)
+
+document.title = "dynamic from js"
+
+
+let sqr = num => num*num
+
+window.name = "suyash"
+var person = {
+    name: "yash",
+    hobbies:['tech', 'gym'],
+    printHobbies : function(){
+        this.hobbies.forEach((hobbyName)=>{
+            console.log(`${this.name} has hobby of ${hobbyName}`)
+        })
+    }
+}
+
+
+person.printHobbies()
